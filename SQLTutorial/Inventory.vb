3895a7c2429e407ef87776c3cf1f5aff69d753ba
Ngoy Banza <ngoyb@Fulcrum.local>
2019-02-13 16:13:29 +0200

    Add search using the textArea and Search Button

--- SQLTutorial/Inventory.vb
+++ SQLTutorial/Inventory.vb
@@ -18,8 +18,16 @@ Public Class Inventory
 
     End Sub
 
+    Private Sub FindItem()
+        SQL.AddParam("@item", "%" & txtSearch.Text & "%")
+        LoadGrid("SELECT * FROM Products WHERE PartNo LIKE @item;")
+    End Sub
+    
     Private Sub Inventory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MdiParent = Form1
         LoadGrid("SELECT PartNo FROM Products WHERE PartNo LIKE '%MOW%';")
+    End Sub
+     Private Sub cmdSearch_Click(sender As Object, e As EventArgs) Handles cmdSearch.Click
+        FindItem()
     End Sub
 End Class
