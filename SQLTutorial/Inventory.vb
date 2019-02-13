34af9a10f4801384a182133eb5fba6dce58e6c69
Ngoy Banza <ngoyb@Fulcrum.local>
2019-02-13 15:58:13 +0200

    Making the query dynamic

--- SQLTutorial/Inventory.vb
+++ SQLTutorial/Inventory.vb
@@ -3,8 +3,15 @@
 Public Class Inventory
     Public SQL As New SQLControl
 
-    Public Sub LoadGrid()
-        SQL.ExecuteQuery("SELECT * FROM Products;")
+    Public Sub LoadGrid(Optional Query As String = "")
+        If Query = "" Then
+            SQL.ExecuteQuery("SELECT * FROM Products;")
+
+        Else
+            SQL.ExecuteQuery(Query)
+        End If
+
+        'Error Handling
         If SQL.HasException(True) Then Exit Sub
 
         dgvData.DataSource = SQL.DBDT
@@ -13,6 +20,6 @@ Public Class Inventory
 
     Private Sub Inventory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MdiParent = Form1
-        LoadGrid()
+        LoadGrid("SELECT PartNo FROM Products WHERE PartNo LIKE '%MOW%';")
     End Sub
 End Class
