90b3ccec76c1121769181cdf38e88348499f0905
Ngoy Banza <dngoyb@gmail.com>
2019-03-07 15:35:48 +0200

    Edit User phone designed

---
+++ SQLTutorial/EditUser.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EditUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFilter = New System.Windows.Forms.TextBox()
+        Me.lbUsers = New System.Windows.Forms.ListBox()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.cbAdmin = New System.Windows.Forms.CheckBox()
+        Me.cbActive = New System.Windows.Forms.CheckBox()
+        Me.cmdSubmit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtFilter
+        '
+        Me.txtFilter.Location = New System.Drawing.Point(12, 29)
+        Me.txtFilter.Name = "txtFilter"
+        Me.txtFilter.Size = New System.Drawing.Size(197, 22)
+        Me.txtFilter.TabIndex = 0
+        '
+        'lbUsers
+        '
+        Me.lbUsers.FormattingEnabled = True
+        Me.lbUsers.ItemHeight = 16
+        Me.lbUsers.Location = New System.Drawing.Point(13, 58)
+        Me.lbUsers.Name = "lbUsers"
+        Me.lbUsers.Size = New System.Drawing.Size(196, 260)
+        Me.lbUsers.TabIndex = 1
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(497, 429)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(8, 4)
+        Me.CheckedListBox1.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cmdSubmit)
+        Me.GroupBox1.Controls.Add(Me.cbActive)
+        Me.GroupBox1.Controls.Add(Me.cbAdmin)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtPass)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtUser)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtID)
+        Me.GroupBox1.Location = New System.Drawing.Point(228, 29)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(387, 289)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Member Details"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(48, 29)
+        Me.txtID.Name = "txtID"
+        Me.txtID.ReadOnly = True
+        Me.txtID.Size = New System.Drawing.Size(79, 22)
+        Me.txtID.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ID: "
+        '
+        'txtUser
+        '
+        Me.txtUser.Location = New System.Drawing.Point(123, 76)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.ReadOnly = True
+        Me.txtUser.Size = New System.Drawing.Size(196, 22)
+        Me.txtUser.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Username: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 121)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Password: "
+        '
+        'txtPass
+        '
+        Me.txtPass.Location = New System.Drawing.Point(123, 121)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(196, 22)
+        Me.txtPass.TabIndex = 4
+        '
+        'cbAdmin
+        '
+        Me.cbAdmin.AutoSize = True
+        Me.cbAdmin.Location = New System.Drawing.Point(143, 160)
+        Me.cbAdmin.Name = "cbAdmin"
+        Me.cbAdmin.Size = New System.Drawing.Size(69, 21)
+        Me.cbAdmin.TabIndex = 6
+        Me.cbAdmin.Text = "Admin"
+        Me.cbAdmin.UseVisualStyleBackColor = True
+        '
+        'cbActive
+        '
+        Me.cbActive.AutoSize = True
+        Me.cbActive.Location = New System.Drawing.Point(143, 187)
+        Me.cbActive.Name = "cbActive"
+        Me.cbActive.Size = New System.Drawing.Size(68, 21)
+        Me.cbActive.TabIndex = 7
+        Me.cbActive.Text = "Active"
+        Me.cbActive.UseVisualStyleBackColor = True
+        '
+        'cmdSubmit
+        '
+        Me.cmdSubmit.Location = New System.Drawing.Point(143, 225)
+        Me.cmdSubmit.Name = "cmdSubmit"
+        Me.cmdSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSubmit.TabIndex = 8
+        Me.cmdSubmit.Text = "Button1A"
+        Me.cmdSubmit.UseVisualStyleBackColor = True
+        '
+        'EditUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(627, 327)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.lbUsers)
+        Me.Controls.Add(Me.txtFilter)
+        Me.Name = "EditUser"
+        Me.Text = "EditUser"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFilter As TextBox
+    Friend WithEvents lbUsers As ListBox
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cmdSubmit As Button
+    Friend WithEvents cbActive As CheckBox
+    Friend WithEvents cbAdmin As CheckBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtID As TextBox
+End Class
